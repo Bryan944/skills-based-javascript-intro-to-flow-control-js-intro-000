e1b2c5dc50e6dd66fafe3caf4aed4e6bbeec9649
cheerful-response-5821 <bryan.nguyen2494@gmail.com>
2020-04-04 22:04:40 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,37 @@
 function basicTeenager(age) {
-
+    if (age <= 19) {
+      return 'you are teenager';
+    }
 }
-
 function teenager(age) {
+  if(age >= 13 && age <= 19) {
+    return 'you are teenager'
+  } else{
+    return 'you are not teenager'
+  }
+} console.log(teenager(15));
 
-}
 
 function ageChecker(age) {
+  if(age >= 13 && age <= 19) {
+    return 'you are teenager!'
+  } else if( age <=12) {
+    return 'you are kid';
+  } else {
+    return 'you are grownup'
+  }
+} console.log(ageChecker(15));
 
-}
+
 
 function ternaryTeenager(age) {
-
+    
+  return  age >= 13 && age <= 19 ? 'you are teenager' : 'you are not teenager';
 }
+console.log(ternaryTeenager(16));
 
 function switchAge(age) {
-
+    
+        
+    
 }
